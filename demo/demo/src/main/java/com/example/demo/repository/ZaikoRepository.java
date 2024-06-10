c54581d0c6d59679cf540d401146dfeac0dec78b
Adilic <zhouqi2580@gmail.com>
2024-06-10 21:31:38 +0900

    start

--- demo/demo/src/main/java/com/example/demo/repository/ZaikoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Zaiko;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZaikoRepository extends JpaRepository<Zaiko, Integer> {
-}
